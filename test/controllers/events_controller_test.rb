5aee4eb8829d29a1bc8640c4e4a6e10ae9dfe4b6
Rishi Jain <rishi@joshsoftware.com>
2016-09-12 12:28:22 +0530

    test for events index and create added

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -4,4 +4,18 @@ class EventsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+    bill = Event.create(name: "first event")
+    sign_in users(:amar)
+    get :index
+    assert_response :success
+  end
+
+  test "should create an event" do
+    sign_in users(:amar)
+    assert_difference('Event.count') do
+      post :create, {event: {name: "new event"}}
+    end
+    assert_redirected_to events_path
+  end
 end
